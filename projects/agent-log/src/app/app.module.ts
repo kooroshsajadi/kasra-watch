b3bf60536abcba03e1c99ef4ea46ab5365215cbd
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2021-01-09 17:47:59 +0330

    initial

--- projects/agent-log/src/app/app.module.ts
+++ projects/agent-log/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RTL } from '@progress/kendo-angular-l10n';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GridModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+
+   // Enable Right-to-Left mode for Kendo UI components
+  providers: [
+    { provide: RTL, useValue: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
