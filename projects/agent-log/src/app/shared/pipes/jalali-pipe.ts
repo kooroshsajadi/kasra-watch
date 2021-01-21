4c527b6f2faa45f6ce93a800e6c73539c067eb1e
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2021-01-21 10:14:48 +0330

    Cleaned the code.

--- projects/agent-log/src/app/shared/pipes/jalali-pipe.ts
+++ projects/agent-log/src/app/shared/pipes/jalali-pipe.ts
@@ -6,7 +6,6 @@ import * as moment from 'jalali-moment';
   })
   export class JalaliPipe implements PipeTransform {
     transform(value: any, args?: any): any {
-      debugger
       let MomentDate = moment(value, 'YYYY/MM/DD');
       return MomentDate.locale('fa').format('YYYY/M/D HH:mm:ss');
     }
