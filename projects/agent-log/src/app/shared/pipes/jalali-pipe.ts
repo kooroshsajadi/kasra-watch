5b0001a6642615d8fffd0baabb4c01b7e1360466
Kourosh Sajjadi <kourosh_sajjadi@outlook.com>
2021-03-10 18:17:53 +0330

    Fixed some parts of the UI. Maintained the stauts column value. Altered the date pipe.

--- projects/agent-log/src/app/shared/pipes/jalali-pipe.ts
+++ projects/agent-log/src/app/shared/pipes/jalali-pipe.ts
@@ -6,7 +6,25 @@ import * as moment from 'jalali-moment';
   })
   export class JalaliPipe implements PipeTransform {
     transform(value: any, args?: any): any {
-      let MomentDate = moment(value, 'YYYY/MM/DD');
-      return MomentDate.locale('fa').format('YYYY/M/D HH:mm:ss');
+      let momentDate = moment(value, 'YYYY/MM/DD');
+      let now = moment();
+      now = moment(now, 'YYYY/MM/DD');
+      let daysDifference = now.diff(momentDate, 'days');
+      let hoursDifference = now.diff(momentDate, 'hours');
+      let minutesDifference = now.diff(momentDate, 'minutes');
+      if(daysDifference <= 20 && daysDifference >= 1) {
+        // return momentDate.locale('fa').format('HH:mm:ss') + `روز قبل ${daysDifference}`
+        return `${daysDifference} روز قبل ` + momentDate.locale('fa').format('HH:mm:ss')
+      }
+      else if(hoursDifference <= 23 && hoursDifference >= 1){
+        return `${hoursDifference} ساعت قبل`
+      }
+      else if(minutesDifference <= 59 && minutesDifference >= 1){
+        return `${minutesDifference} دقیقه قبل`
+      }
+      else if(minutesDifference < 1){
+        return "چند لحطه پیش"
+      }
+      return momentDate.locale('fa').format('YYYY/M/D HH:mm:ss');
     }
   }
